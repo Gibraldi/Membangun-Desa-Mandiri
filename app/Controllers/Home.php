7dfa66bbcd541a8be70e631d5d537b4f1656b9e2
Gibraldi <gibral_radifan@yahoo.com>
2024-05-07 19:06:34 +0700

    update location

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+         echo view("dash_header");
+         echo view('dashboard_view');
+         echo view("dash_footer");
     }
+
+    public function table()
+    {
+         echo view("dash_header");
+         echo view('table');
+         echo view("dash_footer");
+    }
+
 }
---
+++ app/Views/table.php
@@ -0,0 +1,16 @@
+
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+        
+          <div id="map" style="width: 1600px; height: 800px;"></div>
+          
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+ 
